921340c98db01ac7c6baea2e792cb365b849a467
seongmin2223 <khyuno9@naver.com>
2024-06-16 22:29:14 +0900

    엑셀

--- src/main/java/com/mypagingboard/demo/DemoApplication.java
+++ src/main/java/com/mypagingboard/demo/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyMarketControllerApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyMarketControllerApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
